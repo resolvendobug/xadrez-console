41d5a2de76f67ac05accad08a268edb83cfc57da
Renato <renato@resolvendobug.com.br>
2023-02-28 16:26:20 -0300

    Add Pecas in Tabuleiro

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -3,14 +3,14 @@ namespace tabuleiro
 {
     internal class Peca
     {
-        public Posicao posicao { get; set; }
+        public Posicao? posicao { get; set; }
         public Cor cor { get; protected set; }
         public int qteMovimentos { get; set; }
         public Tabuleiro tab { get; set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        public Peca(Tabuleiro tab, Cor cor)
         {
-            this.posicao = posicao;
+            this.posicao = null;
             this.cor = cor;
             this.tab = tab;
             this.qteMovimentos = 0;
