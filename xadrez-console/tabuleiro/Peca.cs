99da56f1370eb6a29fab34042ba39089f419ad39
Renato <renato@resolvendobug.com.br>
2023-03-06 11:12:40 -0300

    fix; change method name movimentoPossivel

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -42,7 +42,7 @@ namespace tabuleiro
             return false;
         }
 
-        public bool podeMoverPara(Posicao pos)
+        public bool movimentoPossivel(Posicao pos)
         {
             return movimentosPossiveis()[pos.linha, pos.coluna];
         }
--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -97,7 +97,7 @@ namespace xadrez
 
         public void validarPosicaoDeDestino(Posicao origem,Posicao destino)
         {
-            if (!tab.peca(origem).podeMoverPara(destino))
+            if (!tab.peca(origem).movimentoPossivel(destino))
             {
                 throw new TabuleiroExeption("Posição de destino invalida!");
             }
